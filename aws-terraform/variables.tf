daae1ff5d929f9a4d6c86d9bbc6beef9bbaed07a
Anatolii Kerod <tolikkerod@gmail.com>
2025-07-19 21:22:18 +0300

    Add terraform variables

---
+++ aws-terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  type = "string"
+  default = "ea-central-1"
+}
+
+variable "project_name" {
+  type = "string"
+  default = "DevOps Project"
+}
+
+variable "instance_type" {
+  type = "string"
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  type = string
+  default = "ami-0af9b40b1a16fe700"
+}
+
+variable "ssh_key_name" {
+  type = string
+  default = "devops-project-key"
+}
+
+variable "vpc_cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "publick_subnet_cidr_block" {
+  type = string
+  default = "10.0.10.0/24"
+}
